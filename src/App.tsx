c9519a8532674bb88a51fdf827ddfd61a241485d
Cristopher André <cristopher@weg.net>
2026-01-15 14:16:58 -0300

    add CVA Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Text from './components/text';
 
 export default function App() {
 
   return (
-    <div className="text-pink-base">
-      Hello
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base"> Hello, Worlds!</Text>
+
+      <Text className="text-green-base"> Hello, Worlds!</Text>
+
+      <Text variant="body-md-bold"> Hello, Worlds!</Text>
     </div>
-  )
+
+
+  );
+
 }
